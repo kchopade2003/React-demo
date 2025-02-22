2868643971d2533ff49da509c61b34b71525247f
kchopade2003 <ketanchopade3@gmail.com>
2025-02-22 11:16:09 +0530

    Third commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Intro1 from './components/Intro1';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
       <h2>VESITians</h2>
+      <Intro1></Intro1>
     </div>
   );
 }
---
+++ src/components/Intro1.js
@@ -0,0 +1,23 @@
+// functional components
+
+import React from 'react';
+
+// function Intro1(){
+//     return(
+//         <div>
+//             <h1>Hello,</h1>
+//             <h2>My name is Ketan.</h2>
+//         </div>
+//     )
+// }
+
+const Intro1 = () => {
+    return(
+        <div>
+            <h1>Hello,</h1>
+            <h2>My name is Ketan.</h2>
+        </div>
+    )
+}
+
+export default Intro1;
